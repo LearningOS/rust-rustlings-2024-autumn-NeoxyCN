812efa50208ab9a296f9cd7bd401aa35c0fa7d68
Neoxy <1007050740@qq.com>
2024-10-20 05:43:10 +0000

    string

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,23 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let i = String::from(input);
+    String::from(input.trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let i =String::from(input);
+    i+" world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let i = String::from(input);
+    i.replace("cars","balloons")
 }
 
 #[cfg(test)]
